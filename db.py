dc3794b3b5536d87ad5df934beb9e898e45a7b8b
Pavel Drovnin <pavel98rus@gmail.com>
2023-08-20 03:33:19 +0300

    cats color

---
+++ db.py
@@ -0,0 +1,46 @@
+import asyncpg
+import asyncio
+
+from settings import config
+
+
+async def create_connection():
+    database, user, password, host, port = config.values()
+    try:
+        conn = await asyncpg.connect(
+            user=user, host=host, port=port, database=database,
+            password=password
+        )
+    except Exception as e:
+        raise e
+    return conn
+
+
+async def cats_color_count():
+    """Подсчет количества котов по цветам"""
+    try:
+        conn = await create_connection()
+        await conn.execute(
+            '''INSERT INTO cat_colors_info (color, count)
+            SELECT color, count(color) FROM cats GROUP BY color;'''
+        )
+    except Exception as e:
+        raise e
+
+
+async def cats_stats():
+    try:
+        conn = await create_connection()
+        await conn.execute(
+            '''INSERT INTO cats_stat (tail_length_mean, tail_length_median,
+            tail_length_mode, whiskers_length_mean, whiskers_length_median,
+            whiskers_length_mode)
+            SELECT AVG(tail_length), 
+            '''
+        )
+    except Exception as e:
+        raise e
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(cats_color_count())
---
+++ settings.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import yaml
+
+
+BASE_DIR = Path(__file__).parent
+config_path = BASE_DIR / 'config.yaml'
+
+
+def get_yaml(path: Path) -> dict:
+    with open(path) as f:
+        return yaml.safe_load(f)
+
+
+config = get_yaml(config_path)
