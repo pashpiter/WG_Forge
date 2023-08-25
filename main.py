af0c7b7509f15990dd6fe75696ce06abf95f4264
Pavel Drovnin <pavel98rus@gmail.com>
2023-08-25 04:01:57 +0300

    new post route

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiohttp import web
 
-from db import cats_list
+from db import cats_list, new_cat_to_db
+from validation import validation_cat, validation_request
 
 
 routes = web.RouteTableDef()
@@ -14,8 +15,22 @@ async def ping(request):
 
 @routes.get('/cats')
 async def cats(requset):
-    all_cats, status = await cats_list(requset)
+    params_or_text, status = await validation_request(requset)
+    if status == 400:
+        return web.Response(text=params_or_text, status=status)
+    all_cats, status = await cats_list(*params_or_text)
     return web.json_response(data=all_cats, status=status)
+
+
+@routes.post('/cat')
+async def post_cat(request):
+    new_cat = await request.json()
+    text, status = await validation_cat(new_cat)
+    if status == 400:
+        return web.Response(text=text, status=status)
+    status = await new_cat_to_db(new_cat)
+    return web.Response(text='New cat sucsessfuly added', status=status)
+
 
 if __name__ == '__main__':
     app.add_routes(routes)
